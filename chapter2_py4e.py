2da235749fb01f2e0f2cb630d47314459ecd0f13
Mariya Amelcheva <ma.amelcheva@gmail.com>
2023-01-19 15:39:01 +0100

    test commit 1

---
+++ chapter2_py4e.py
@@ -0,0 +1,28 @@
+x = input('enter your name: ')
+print('Hello', x)
+
+______________________________________
+
+hours = input('enter hours: ')
+rate = input('enter rate: ')
+pay = float(hours) * float(rate)
+print(pay)
+
+_________________________________________
+
+width = 17
+height = 12.0
+print(width // 2)
+
+print(width / 2.0)
+
+print(height / 3)
+
+print(1 + 2 * 5)
+
+_________________________________________
+
+x = input('enter temperature celcius: ')
+c = float(x)
+f = c * 9 / 5 + 32
+print('фаренгейт: ', f, '°F')
